b30749602c60bfed9f4f4ce4b232fa2318dd5512
Rasketch-ai <71091407+Rasketch-ai@users.noreply.github.com>
2020-11-22 21:56:02 +0300

    Add files via upload

--- rabin_carp.py
+++ rabin_carp.py
@@ -1,6 +1,8 @@
-text = "End of passion play, crumbling away I'm your source of self-destruction Veins that pump with fear,sucking darkest clear Leading on your deaths' construction Taste me you will see More is all you need Dedicated to How I'm killing you Obey your master Master Master of puppets I'm pulling your strings Twisting your mind and smashing your dreams Blinded by me, you can't see a thing Just call my name, 'cause I'll hear you scream Master Master Just call my name, 'cause I'll hear you scream Master Master, yeah Needlework the way, never you betray Life of death becoming clearer Pain monopoly, ritual misery Chop your breakfast on a mirror Taste me you will see More is all you need You're dedicated to How I'm fucking you Obey your master Master Master of puppets I'm pulling your strings Twisting your mind and smashing your fucking dreams Blinded by me, you can't see a thing Just call my name, 'cause I'll hear you scream Master Master Just call my name, 'cause I'll hear you scream Master Master"
-pattern = "Master"
+''' Что-то чтобы пайлинт не ругался :) '''
+TEXT = "Master of puppets I'm pulling your strings Twisting your mind and smashing your dreams"
+PATTERN = "Master"
 def rabin_carp(text,pattern):
+    ''' Алгоритм Рабина-Карпа '''
     result = ([])
     pattern_sum = 0
     main_sum = 0
@@ -9,13 +11,10 @@ def rabin_carp(text,pattern):
     for k in range (0, len(pattern)):
         main_sum += ord(text[k])
     for k in range (len(text)-len(pattern)):
-        if main_sum == pattern_sum :
-            if text.startswith(pattern,k) : result.append(k) 
+        if main_sum == pattern_sum:
+            if text.startswith(pattern,k):
+                result.append(k)
         main_sum += (ord(text[k + len(pattern)]) - ord(text[k]))
-    return (result)
-result = rabin_carp(text,pattern)
-if (result != []): print ("Подстрока",pattern,"Найдена на позициях",result)
-
-
-         
-
+    return result
+if rabin_carp(TEXT,PATTERN) != []:
+    print ("Подстрока",PATTERN,"Найдена на позициях",rabin_carp(TEXT,PATTERN))
